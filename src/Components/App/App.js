f2515bad294fed264ffc2507e8c20e509e82a480
Katherine Blaine <blaine.katherine12@gmail.com>
2023-07-18 14:55:50 -0500

    feat: Assign mock data to app state on app component load

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+import { data } from '../../data/data';
+import { useState, useEffect } from 'react';
 
 const App = () => {
+  const [gameData, setGameData] = useState({})
+
+  useEffect(() => {
+    setGameData(data)
+  }, [])
+
   return (
-    <>
+    <div className='home-page'>
       <h1>WordHive</h1>
-    </>
+      <button>Let's Play!</button>
+    </div>
   );
 }
 
