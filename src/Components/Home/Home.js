8bc0e8015e0396c13e2e3b59789a5f5006156cb0
Katherine Blaine <blaine.katherine12@gmail.com>
2023-07-20 15:11:03 -0500

    feat: Delete unnecessary state in home component

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,20 +1,10 @@
 import { data } from '../../data/data';
-import { useState, useEffect } from 'react';
 
 const Home = ({ letsPlay }) => {
-  const [newGame, setNewGame] = useState({})
-
-  useEffect(() => {
-    setNewGame(data)
-  })
-
   return (
     <div className='home-page'>
       <h1>WordHive</h1>
-      <button onClick={() => {
-        letsPlay(data)
-        console.log(newGame)
-        }}>Let's Play!</button>
+      <button onClick={() => letsPlay(data)}>Let's Play!</button>
     </div>
   )
 }
