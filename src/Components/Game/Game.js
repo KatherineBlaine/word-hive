40056701bd2911cfe88576e19b8441416228578e
Katherine Blaine <blaine.katherine12@gmail.com>
2023-07-20 16:41:22 -0500

    feat: Design basic gameboard layout

--- src/Components/Game/Game.js
+++ src/Components/Game/Game.js
@@ -1,26 +1,28 @@
 import { useState, useEffect } from "react";
+import './Game.css'
 
 const Game = ({ data }) => {
   const [letters, setLetters] = useState([])
   const [centerLetter, setCenterLetter] = useState('')
 
   useEffect(() => {
-    setLetters(data.letters.split(''))
-    setCenterLetter(data.center)
+    setLetters(data.letters.toUpperCase().split(''))
+    setCenterLetter(data.center.toUpperCase())
   }, [])
 
   return (
     <>
-      <h1>New Game</h1>
-      {letters.length > 0 && <div className="letters">
-        <p className="letter">{letters[0]}</p>
-        <p className="letter">{letters[1]}</p>
-        <p className="letter">{letters[2]}</p>
-        <p className="letter">{letters[3]}</p>
-        <p className="letter">{letters[4]}</p>
-        <p className="letter">{letters[5]}</p>
-        <p className="center">{centerLetter}</p>
+    <div className="game-board">
+      {letters.length > 0 && <div className="all-letters">
+        <p id="letter1" className="letter">{letters[0]}</p>
+        <p id="letter2" className="letter">{letters[1]}</p>
+        <p id="letter3" className="letter">{letters[2]}</p>
+        <p id="letter4" className="letter">{letters[3]}</p>
+        <p id="letter5" className="letter">{letters[4]}</p>
+        <p id="letter6" className="letter">{letters[5]}</p>
+        <p id="center" className="letter">{centerLetter}</p>
       </div>}
+    </div>
     </>
   )
 }
