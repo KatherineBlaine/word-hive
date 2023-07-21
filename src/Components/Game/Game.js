22f2609ece1eee0cff1cc402962f50b5afa84426
Katherine Blaine <blaine.katherine12@gmail.com>
2023-07-21 11:57:20 -0500

    feat: Create controlled form in game component

--- src/Components/Game/Game.js
+++ src/Components/Game/Game.js
@@ -4,6 +4,7 @@ import './Game.css'
 const Game = ({ data }) => {
   const [letters, setLetters] = useState([])
   const [centerLetter, setCenterLetter] = useState('')
+  const [word, setWord] = useState([])
 
   useEffect(() => {
     setLetters(data.letters.toUpperCase().split(''))
@@ -14,14 +15,15 @@ const Game = ({ data }) => {
     <>
     <div className="game-board">
       {letters.length > 0 && <div className="all-letters">
-        <p id="letter1" className="letter">{letters[0]}</p>
-        <p id="letter2" className="letter">{letters[1]}</p>
-        <p id="letter3" className="letter">{letters[2]}</p>
-        <p id="letter4" className="letter">{letters[3]}</p>
-        <p id="letter5" className="letter">{letters[4]}</p>
-        <p id="letter6" className="letter">{letters[5]}</p>
-        <p id="center" className="letter">{centerLetter}</p>
+        <p id="letter1" className="letter" onClick={() => setWord([...word, letters[0]])}>{letters[0]}</p>
+        <p id="letter2" className="letter" onClick={() => setWord([...word, letters[1]])}>{letters[1]}</p>
+        <p id="letter3" className="letter" onClick={() => setWord([...word, letters[2]])}>{letters[2]}</p>
+        <p id="letter4" className="letter" onClick={() => setWord([...word, letters[3]])}>{letters[3]}</p>
+        <p id="letter5" className="letter" onClick={() => setWord([...word, letters[4]])}>{letters[4]}</p>
+        <p id="letter6" className="letter" onClick={() => setWord([...word, letters[5]])}>{letters[5]}</p>
+        <p id="center" className="letter" onClick={() => setWord([...word, centerLetter])}>{centerLetter}</p>
       </div>}
+      <input value={word.join('')} onChange={(event) => setWord(event.target.value)}></input>
     </div>
     </>
   )
